cb3a6d76edf3cc46fcd54666c0b0cbf44e8a4419
simonecalzolaro <simone.calzolaro@mail.polimi.it>
2023-03-19 22:29:26 +0100

    Prova da intelliJ

---
+++ project_eng_soft_2023/src/test/java/model/BookshelfTest.java
@@ -0,0 +1,12 @@
+package model;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+class BookshelfTest {
+
+}
