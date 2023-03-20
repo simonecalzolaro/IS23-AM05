6272b46038813c0fca7a24f81c954f0e6498ceeb
simonecalzolaro <simone.calzolaro@mail.polimi.it>
2023-03-20 22:18:53 +0100

    Correzione metodi EOGToken

--- project_eng_soft_2023/src/main/java/model/Tile.java
+++ project_eng_soft_2023/src/main/java/model/Tile.java
@@ -11,23 +11,4 @@ public enum Tile {
     EMPTY,
     NOTAVAILABLE;
 
-    private int x;
-    private int y;
-
-
-    public void setX(int x){
-        this.x = x;
-    }
-
-    public void setY(int y){
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
 }
