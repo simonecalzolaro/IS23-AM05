8e6b2cc146938031fddb33d93dbd0893fa392fb1
simonecalzolaro <simone.calzolaro@mail.polimi.it>
2023-03-20 22:17:35 +0100

    Bookshelf + Board update

--- project_eng_soft_2023/src/main/java/model/Tile.java
+++ project_eng_soft_2023/src/main/java/model/Tile.java
@@ -11,22 +11,5 @@ public enum Tile {
     EMPTY,
     NOTAVAILABLE;
 
-    private int x;
-    private int y;
 
-    public void setX(int x){
-        this.x = x;
-    }
-
-    public void setY(int y){
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
 }
