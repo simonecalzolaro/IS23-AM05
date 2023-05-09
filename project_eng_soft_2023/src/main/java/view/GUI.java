bd88878afe58b425537d69fd41ec8f5a983bd32c
Gabriele Clara Di Gioacchino <gabriele.claradigioacchino@mail.polimi.it>
2023-05-09 15:54:38 +0200

    view, tui e gui

---
+++ project_eng_soft_2023/src/main/java/view/GUI.java
@@ -0,0 +1,4 @@
+package view;
+
+public class GUI implements View{
+}
---
+++ project_eng_soft_2023/src/main/java/view/TUI.java
@@ -0,0 +1,35 @@
+package view;
+
+public class TUI implements View{
+
+    private enum ColorCLI{
+        RESET("\u001B[0m"),
+
+        GREEN("\u001B[32m"),
+        BLUE("\u001B[34m"),
+        WHITE("\u001B[37m"),
+        PINK("\u001B[35m"),
+        YELLOW("\u001B[33m"),
+        LIGHTBLUE("\u001B[36m"),
+
+        BLACK_BACKGROUND("\u001B[40m"),
+        GREEN_BACKGROUND("\u001B[42m"),
+        BLUE_BACKGROUND("\u001B[44m"),
+        WHITE_BACKGROUND("\u001B[47m"),
+        PINK_BACKGROUND("\u001B[45m"),
+        YELLOW_BACKGROUND("\u001B[43m"),
+        LIGHTBLUE_BACKGROUND("\u001B[46m");
+
+        private final String color;
+
+        ColorCLI(String color){
+            this.color = color;
+        }
+
+        public String toString(){
+            return color;
+        }
+    }
+
+
+}
