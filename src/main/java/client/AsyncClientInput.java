30e79cba9309f388375d24ad5607c0e6e94e5262
simonecalzolaro <simone.calzolaro@mail.polimi.it>
2023-06-26 15:57:06 +0200

    AsyncClientInput exception fixed

--- src/main/java/client/AsyncClientInput.java
+++ src/main/java/client/AsyncClientInput.java
@@ -8,7 +8,7 @@ import java.rmi.RemoteException;
 import java.util.Map;
 import model.Tile;
 
-public class AsyncClientInput implements Runnable{
+public class AsyncClientInput implements Runnable {
 
 
     SocketClient socketClient;
@@ -16,12 +16,12 @@ public class AsyncClientInput implements Runnable{
     private Stream outClient;
     private Stream inClient;
 
-    public AsyncClientInput(SocketClient socketClient){
+    public AsyncClientInput(SocketClient socketClient) {
         this.socketClient = socketClient;
         setStreams();
     }
 
-    private void setStreams(){
+    private void setStreams() {
         outClient = socketClient.getOutputStream();
         inClient = socketClient.getInputStream();
     }
@@ -29,51 +29,57 @@ public class AsyncClientInput implements Runnable{
 
     /**
      * Receives request from the server, parse the 'Action' field and so choose the action to perform on the client
-     *
      */
-    private void startServer(){
+    private void startServer() {
 
         JSONObject request = new JSONObject();
         JSONObject response = new JSONObject();
 
-        while (true){
+        while (true) {
 
-            if(!request.equals(null)) request.clear();
-            if(!response.equals(null)) response.clear();
+            String Action = null;
 
-            try{
+            request.clear();
+            response.clear();
+
+            try {
                 request = inClient.read();
             } catch (InvalidOperationException e) {
                 System.out.println("AsyncClientInput  --- InvalidOperationException occurred trying to read a new request");
-                e.printStackTrace();
             } catch (IOException e) {
                 System.out.println("AsyncClientInput  --- IOException occurred trying to read a new request");
-                e.printStackTrace();
+
             } catch (ClassNotFoundException e) {
                 System.out.println("AsyncClientInput  --- ClassNotFoundException occurred trying to read a new request");
-                e.printStackTrace();
+
             }
 
-            String Action = (String) request.get("Action");
 
-            switch (Action){
+            try{
+                Action = (String) request.get("Action");
+            } catch (Exception e) {
+                System.out.println("");
+            }
+
+
+            switch (Action) {
 
                 //enterNumberOfPlayers
                 case "askNumberOfPlayers":
-                        new Thread(() -> {
-                            try {
-                                socketClient.enterNumberOfPlayers();
-                            } catch (RemoteException e) {
-                                throw new RuntimeException(e);
-                            }
-                        }).start();
+                    new Thread(() -> {
+                        try {
+                            socketClient.enterNumberOfPlayers();
+                        } catch (RemoteException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }).start();
 
                     break;
 
 
                 //updateBoard
                 case "notifyUpdatedBoard":
-                    try{
+                    try {
 
                         Tile[][] board = (Tile[][]) request.get("Param1");
                         Tile[][] shelf = (Tile[][]) request.get("Param2");
@@ -82,7 +88,7 @@ public class AsyncClientInput implements Runnable{
                         int gameID = (int) request.get("Param5");
 
 
-                        socketClient.updateBoard(board,shelf,map,score);
+                        socketClient.updateBoard(board, shelf, map, score);
                     } catch (RemoteException e) {
                         throw new RuntimeException(e);
                     }
@@ -92,14 +98,14 @@ public class AsyncClientInput implements Runnable{
                 //startPlaying
                 case "notifyStartPlaying":
 
-                    try{
+                    try {
                         int pgcNum = (int) request.get("Param1");
                         Map<Tile, Integer[]> cardMap = (Map<Tile, Integer[]>) request.get("Param2");
                         int cgc1Num = (int) request.get("Param3");
                         int cgc2Num = (int) request.get("Param4");
                         int gameID = (int) request.get("Param5");
 
-                        socketClient.startPlaying(pgcNum,cardMap,cgc1Num,cgc2Num,gameID);
+                        socketClient.startPlaying(pgcNum, cardMap, cgc1Num, cgc2Num, gameID);
                     } catch (RemoteException e) {
                         throw new RuntimeException(e);
                     }
@@ -130,7 +136,7 @@ public class AsyncClientInput implements Runnable{
 
                 //endGame
                 case "notifyEndGame":
-                    try{
+                    try {
                         Map<String, Integer> results = (Map<String, Integer>) request.get("Param1");
 
                         socketClient.theGameEnd(results);
@@ -153,8 +159,8 @@ public class AsyncClientInput implements Runnable{
                     String nick = (String) request.get("Param1");
                     String message = (String) request.get("Param2");
 
-                    try{
-                        socketClient.receiveMessage(nick,message);
+                    try {
+                        socketClient.receiveMessage(nick, message);
                     } catch (RemoteException e) {
                         throw new RuntimeException(e);
                     }
@@ -164,22 +170,22 @@ public class AsyncClientInput implements Runnable{
                 //restore session
                 case "restoreSession":
 
-                   try{
-                       Tile[][] board = (Tile[][]) request.get("Param1");
-                       Tile[][] shelf = (Tile[][]) request.get("Param2");
-                       Map<String, Tile[][]> map = (Map<String, Tile[][]>) request.get("Param3");
-                       int score = (int) request.get("Param4");
-                       int gameID = (int) request.get("Param5");
-                       int pgcNum = (int) request.get("Param6");
-                       Map<Tile, Integer[]> cardMap = (Map<Tile, Integer[]>) request.get("Param7");
-                       int cgc1 = (int) request.get("Param8");
-                       int cgc2 = (int) request.get("Param9");
+                    try {
+                        Tile[][] board = (Tile[][]) request.get("Param1");
+                        Tile[][] shelf = (Tile[][]) request.get("Param2");
+                        Map<String, Tile[][]> map = (Map<String, Tile[][]>) request.get("Param3");
+                        int score = (int) request.get("Param4");
+                        int gameID = (int) request.get("Param5");
+                        int pgcNum = (int) request.get("Param6");
+                        Map<Tile, Integer[]> cardMap = (Map<Tile, Integer[]>) request.get("Param7");
+                        int cgc1 = (int) request.get("Param8");
+                        int cgc2 = (int) request.get("Param9");
 
-                       socketClient.restoreSession(board,shelf,map,score,gameID,pgcNum,cardMap,cgc1,cgc2);
+                        socketClient.restoreSession(board, shelf, map, score, gameID, pgcNum, cardMap, cgc1, cgc2);
 
-                   } catch (Exception e) {
-                       throw new RuntimeException(e);
-                   }
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
                     break;
 
 
@@ -192,20 +198,14 @@ public class AsyncClientInput implements Runnable{
             }
 
 
-
-
-
         }
 
     }
-
-
-
-
 
 
     @Override
     public void run() {
         startServer();
     }
+
 }
